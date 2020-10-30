cfb202c704b714fb201e2d9499db4309281c1d26
nomadcoderkor <nomadcoder.kor@gmail.com>
2020-10-31 01:17:30 +0900

    Updates

---
+++ precache-manifest.71bd21a36868ef40576f6e9e983cc52d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf111f0019375f664ca848393883550",
+    "url": "/wagle/index.html"
+  },
+  {
+    "revision": "c573db96d8214134480d",
+    "url": "/wagle/static/css/main.2ff3d120.chunk.css"
+  },
+  {
+    "revision": "ad52089fc01addedf971",
+    "url": "/wagle/static/js/2.802124f1.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/wagle/static/js/2.802124f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c573db96d8214134480d",
+    "url": "/wagle/static/js/main.960527a5.chunk.js"
+  },
+  {
+    "revision": "d46a745192354c706d63",
+    "url": "/wagle/static/js/runtime-main.b5531c7b.js"
+  }
+]);
